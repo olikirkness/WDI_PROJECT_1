a4bd48467c0a87cfc8acc359895baf968ba8b04e
olikirkness <oli@kirkness.com>
2017-07-01 15:13:13 +0100

    added random words to blocks

--- js/app.js
+++ js/app.js
@@ -100,29 +100,68 @@
 
 //Code______________________________________________________
 
+// , 'rat', 'fat', 'car', 'bus', 'hut', 'but', 'nut', 'cut'
+
 var wordGame = wordGame || {};
 
 wordGame.setUp = function(){
   this.randomTime();
-  this.time = 1000;
+  this.interval = 1000;
+  this.speed = 5000;
+  this.easy = ['all','am','and','at','ball','be','bed','big','book','box','boy','but','came','can','car','cat','come','cow','dad','day','did','do','dog','fat','for','fun','get','go','good','got','had','hat','he','hen','here','him','his','home','hot','if','in','into','is','it','its','let','like','look','man','may','me','mom','my','no','not','of','oh','old','on','one','out','pan','pet','pig','play','ran','rat','red','ride','run','sat','see','she','sit','six','so','stop','sun','ten','the','this','to','top','toy','two','up','us','was','we','will','yes','you'];
+
+  this.medium = ['uidfiw', 'onwdnf', 'haionoidnveft', 'iowef', 'odnfs', 'onfwfo', 'iojfn', 'njwf', 'ojfnwe', 'jwdofwf', 'onwoff'];
+
 };
 
 wordGame.createBlock = function(){
+  this.changeWord();
+
+  clearInterval(this.timer);
+  this.speed = Math.floor(Math.random()*1000+5000);
+  this.interval = Math.floor(Math.random()*2000+500);
+  this.randomTime();
+
+
+};
+
+wordGame.changeWord = function(){
   this.$positionX = Math.floor(Math.random()*300);
   this.$block = $('<div/>').appendTo('.space').addClass('block');
-  this.$block.css({'left': this.$positionX+'px'}).text('hello').animate({'margin-top': '660px'},5000);
-  clearInterval(this.timer);
-  this.time = Math.floor(Math.random()*2000+500);
-  this.randomTime();
+  if(this.easy.length >= 85){
+    console.log(`easy ${this.easy.length}`);
+    this.$easyIndex = Math.floor(Math.random()*((this.easy).length));
+    this.$block.css({'left': this.$positionX+'px'}).text(`${this.easy[this.$easyIndex]}`).animate({'margin-top': '660px'},this.speed);
+    this.easy.splice(this.$easyIndex, 1);
+  }else if (this.medium.length > 0) {
+    console.log(`medium ${this.medium.length}`);
+    this.$mediumIndex = Math.floor(Math.random()*(this.medium).length-1);
+    this.$block.css({'left': this.$positionX+'px'}).text(`${this.medium[this.$mediumIndex]}`).animate({'margin-top': '660px'},this.speed);
+    this.medium.splice(this.$mediumIndex, 1);
+  } else{
+    console.log('finished');
+    this.end();
+  }
 };
 
 wordGame.randomTime = function(){
   this.timer = setInterval(function () {
-
     wordGame.createBlock();
-
-  }, wordGame.time);
+    // wordGame.positionCheck();
+  }, wordGame.interval);
 };
 
+// wordGame.positionCheck = function(){
+//   if ($('div.block').offset().top >= 300){
+//     console.log('hello');
+//     this.$block.css('background-color', 'blue');
+//   }
+//
+// };
+
+wordGame.end = function(){
+  console.log('game over');
+  clearInterval(this.timer);
+};
 
 $(wordGame.setUp.bind(wordGame));
