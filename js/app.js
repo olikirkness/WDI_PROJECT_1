6658f170595e671edb10fb4f976fca38541eac2c
olikirkness <oli@kirkness.com>
2017-07-01 13:54:20 +0100

    made blocks appear at random intervals and positions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,128 @@
-console.log('hello');
+// $(init);
+//
+//
+//
+// function init(){
+//   console.log('');
+//   createBlock();
+// }
+//
+//
+//
+//
+// function createBlock(){
+//
+//   const $positionX = Math.floor(Math.random()*300);
+//   const $block = $('<div/>').appendTo('.space').addClass('block');
+//   console.log($positionX);
+//   $block.css({'left': $positionX+'px'}).text('hello').animate({'margin-top': '460px'},5000);
+//   test();
+//
+// }
+//
+// function test(){
+//   checkBlockPosition();
+// }
+//
+//
+//
+// function checkBlockPosition(){
+//
+//
+//
+//   var $blockPosition = $('div.block').offset();
+//
+//
+// setInterval(function(){
+//     $('p').text('top' + $blockPosition.top);
+//     if( $blockPosition.top > 209){
+//       this.remove();
+//       next();
+//       clear();
+//       console.log('hello')
+//     }
+//     },100);
+//
+//
+//
+//   $('.block').on('click', function(){
+//     $(this).remove();
+//   });
+//
+//   function next(){
+//     if ($('div.block').length ){
+//       console.log('still here');
+//
+//     }else {
+//       this.remove();
+//       clear();
+//       console.log('hello');
+//     }
+//   }
+//
+//   function clear(){
+//     clearInterval();
+//   }
+//
+// }
+
+
+
+// OutLine__________________________________________________
+
+//Blocks to dynamically appear out of frame, and fall vertically
+//Block to contain randomly selected word.
+//Blocks to disappear if that word its typed in the input box.
+//10 points are awarded for every correctly spelt word.
+//Game over if blocks hit the floor.
+
+// MVP______________________________________________________
+
+//Blocks fall at consistant pace at semi-random intervals (i.e. one every 3-6 seconds).
+//An array of easy, medium and hard words are stored and after the user completed (x) number of words, the difficulty of the words appearing increases.
+//Increment score for every word completed.
+//Game over if the blocks hit the floor.
+
+//Extra_____________________________________________________
+
+//Highlight correctly inputted letters green.
+//Increase the speed of falling.
+//Give blocks aethetic styling.
+//Allow for score to be stored as highscore (while browser isnt refreshed).
+//Create game name and logo.
+
+//PsuedoCode________________________________________________
+
+//Blocks of fixed width and height appear at random intervals and y positions and fall.
+//Y positioning determined by 'left': $positionX+'px` where $positionX is determined by math.random multiplied by the width of the game screen minus the width of the block.
+// Falling speed is based on level and is determined at an increment based on this.
+// Speed will equal ~(max-interval) - math.random x level~
+
+//Code______________________________________________________
+
+var wordGame = wordGame || {};
+
+wordGame.setUp = function(){
+  this.randomTime();
+  this.time = 1000;
+};
+
+wordGame.createBlock = function(){
+  this.$positionX = Math.floor(Math.random()*300);
+  this.$block = $('<div/>').appendTo('.space').addClass('block');
+  this.$block.css({'left': this.$positionX+'px'}).text('hello').animate({'margin-top': '660px'},5000);
+  clearInterval(this.timer);
+  this.time = Math.floor(Math.random()*2000+500);
+  this.randomTime();
+};
+
+wordGame.randomTime = function(){
+  this.timer = setInterval(function () {
+
+    wordGame.createBlock();
+
+  }, wordGame.time);
+};
+
+
+$(wordGame.setUp.bind(wordGame));
